5a3d68ea7dfcb52d51a3a7fb97d0c34c6121e296
seifer2901 <gianni2901@gmail.com>
2020-10-27 22:21:16 +0100

    Created ShakespeareTranslationService - Not implemented

---
+++ src/PokemonTranslator.Providers/Translation/Shakespeare/ShakespeareTranslationService.cs
@@ -0,0 +1,21 @@
+﻿using PokemonTranslator.Abstractions.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonTranslator.Services.Translation.Shakespeare
+{
+	/// <summary>
+	/// Shakespeare translation service
+	/// Wrapper to API https://funtranslations.com/shakespeare
+	/// TO DO: not implemented
+	/// </summary>
+	public class ShakespeareTranslationService : ITranslationService
+	{
+		public Task<string> Translate(string text)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
